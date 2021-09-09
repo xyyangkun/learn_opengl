685b83c8d276c3d610d2781f49389414be0a3a5f
yangkun <xyyangkun@163.com>
2021-09-09 09:37:02 +0800

    add first sample

---
+++ main.cpp
@@ -0,0 +1,42 @@
+/******************************************************************************
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  test opengl
+ *    g++ main.cpp -o firstOpenGlApp -lglut -lGLU -lGL
+ *
+ *        Version:  1.0
+ *        Created:  2021年09月05日 10时47分07秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  yangkun (yk)
+ *          Email:  xyyangkun@163.com
+ *        Company:  yangkun.com
+ *
+ *****************************************************************************/
+#include <GL/glut.h>
+
+void displayMe(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POLYGON);
+        glVertex3f(0.5, 0.0, 0.5);
+        glVertex3f(0.5, 0.0, 0.0);
+        glVertex3f(0.0, 0.5, 0.0);
+        glVertex3f(0.0, 0.0, 0.5);
+    glEnd();
+    glFlush();
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE);
+    glutInitWindowSize(400, 300);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Hello world!");
+    glutDisplayFunc(displayMe);
+    glutMainLoop();
+    return 0;
+}
